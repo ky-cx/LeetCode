ed5aa46a1f0bf944493d3881335f31befa8c2234
Conghui Xu <cx27@st-andrews.ac.uk>
2025-09-06 14:06:41 +0100

    Synchronize existing submission history (1/1)

---
+++ hash-table/two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        number_map = {}
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in number_map:
+                return [i, number_map[diff]]
+            number_map[num] = i
+        return None
---
+++ two-pointers/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        j = 1
+        for i in range(len(nums)-1):
+            if(nums[i]!=nums[i+1]):
+                nums[j] = nums[i+1]
+                j+=1
+        return(j)
+            
+        
