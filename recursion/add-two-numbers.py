26e30f85d0493afc4cce33ab02725c41539cb31a
Conghui Xu <cx27@st-andrews.ac.uk>
2025-09-18 12:26:51 +0100

    LC-add-two-numbers [Runtime: 5 ms; Memory: 17.9 MB]

--- recursion/add-two-numbers.py
+++ recursion/add-two-numbers.py
@@ -1,3 +1,8 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
@@ -5,7 +10,6 @@ class Solution:
         res = dummy
 
         total = carry = 0
-
         while l1 or l2 or carry:
             total = carry
 
@@ -15,10 +19,10 @@ class Solution:
             if l2:
                 total += l2.val
                 l2 = l2.next
-            
+
             num = total % 10
             carry = total // 10
             dummy.next = ListNode(num)
             dummy = dummy.next
-        
+
         return res.next
